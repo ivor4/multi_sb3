abe848d460983cce6e353a0644844b83061720d7
Jaime Bonache <jaime.bonache.iv4@gmail.com>
2024-06-10 19:13:03 +0200

    Evaluate function done

--- jupyterlab/observer.py
+++ jupyterlab/observer.py
@@ -157,8 +157,8 @@ virtual_env_list[1] = Monitor(virtual_env_list[1], LOG_DIR)
 # Create algorithms, by association of its indexed virtual environment with them
 #alg_0 = DQN('CnnPolicy', virtual_env_list[0], tensorboard_log=LOG_DIR)
 #alg_1 = PPO('CnnPolicy', virtual_env_list[1], tensorboard_log=LOG_DIR)
-alg_0 = DQN.load(os.path.join(OPT_DIR, 'best_model_DQN_100000.zip'), env=virtual_env_list[0], tensorboard_log=LOG_DIR)
-alg_1 = PPO.load(os.path.join(OPT_DIR, 'best_model_PPO_100000.zip'), env=virtual_env_list[1], tensorboard_log=LOG_DIR)
+alg_0 = DQN.load(os.path.join(OPT_DIR, 'best_model_DQN_60000.zip'), env=virtual_env_list[0], tensorboard_log=LOG_DIR)
+alg_1 = PPO.load(os.path.join(OPT_DIR, 'best_model_PPO_60000.zip'), env=virtual_env_list[1], tensorboard_log=LOG_DIR)
 
 # Dictionary for DQN will specify DQN model itself, and obs_index 0 to pick first element from real environment return
 alg_collection_0 = {}
@@ -185,8 +185,11 @@ alg_collection = [alg_collection_0, alg_collection_1]
 # Create MultiAlgorithm, this one is in contact with real environment and deffers actions and observation to virtual environments
 model = MultiSB3(real_env, alg_collection, virtual_env_list)
 
-observable_inst = pickle_skip.PickleSkipper(model)
-observable1 = observable_inst.Update()
+#observable_inst = pickle_skip.PickleSkipper(model)
+#observable1 = observable_inst.Update()
 
-model.learn(total_timesteps=100000, callback_alg=callback_list)
+#model.learn(total_timesteps=100000, callback_alg=callback_list)
+
+model.evaluate_multipolicy(render=True, n_eval_episodes=15)
+
 #env.close()
--- stable_baselines3/multi/multi.py
+++ stable_baselines3/multi/multi.py
@@ -21,6 +21,8 @@ from stable_baselines3.common.policies import BasePolicy
 from stable_baselines3.common.type_aliases import GymEnv, MaybeCallback, Schedule
 from stable_baselines3.common.utils import get_linear_fn, get_parameters_by_name, polyak_update
 from stable_baselines3.dqn.policies import CnnPolicy, DQNPolicy, MlpPolicy, MultiInputPolicy, QNetwork
+from stable_baselines3.common import type_aliases
+from stable_baselines3.common.vec_env import DummyVecEnv, VecEnv, VecMonitor, is_vecenv_wrapped
 
 SelfMSB3_Venv = TypeVar("SelfMSB3_Venv", bound="MSB3_VirtualEnv")
 SelfMSB3 = TypeVar("SelfMSB3", bound="MultiSB3")
@@ -460,4 +462,152 @@ class MultiSB3(BaseAlgorithm):
             
         def _setup_model(self) -> None:
             pass
+        
+        def evaluate_multipolicy(
+            self: SelfMSB3,
+            n_eval_episodes: int = 10,
+            deterministic: bool = True,
+            render: bool = False,
+            callback: Optional[Callable[[Dict[str, Any], Dict[str, Any]], None]] = None,
+            reward_threshold: Optional[float] = None,
+            return_episode_rewards: bool = False,
+            warn: bool = True,
+        ) -> Union[Tuple[float, float], Tuple[List[float], List[int]]]:
+            """
+            Runs policy for ``n_eval_episodes`` episodes and returns average reward.
+            If a vector env is passed in, this divides the episodes to evaluate onto the
+            different elements of the vector env. This static division of work is done to
+            remove bias. See https://github.com/DLR-RM/stable-baselines3/issues/402 for more
+            details and discussion.
+
+            .. note::
+                If environment has not been wrapped with ``Monitor`` wrapper, reward and
+                episode lengths are counted as it appears with ``env.step`` calls. If
+                the environment contains wrappers that modify rewards or episode lengths
+                (e.g. reward scaling, early episode reset), these will affect the evaluation
+                results as well. You can avoid this by wrapping environment with ``Monitor``
+                wrapper before anything else.
+
+            :param model: The RL agent you want to evaluate. This can be any object
+                that implements a `predict` method, such as an RL algorithm (``BaseAlgorithm``)
+                or policy (``BasePolicy``).
+            :param env: The gym environment or ``VecEnv`` environment.
+            :param n_eval_episodes: Number of episode to evaluate the agent
+            :param deterministic: Whether to use deterministic or stochastic actions
+            :param render: Whether to render the environment or not
+            :param callback: callback function to do additional checks,
+                called after each step. Gets locals() and globals() passed as parameters.
+            :param reward_threshold: Minimum expected reward per episode,
+                this will raise an error if the performance is not met
+            :param return_episode_rewards: If True, a list of rewards and episode lengths
+                per episode will be returned instead of the mean.
+            :param warn: If True (default), warns user about lack of a Monitor wrapper in the
+                evaluation environment.
+            :return: Mean reward per episode, std of reward per episode.
+                Returns ([float], [int]) when ``return_episode_rewards`` is True, first
+                list containing per-episode rewards and second containing per-episode lengths
+                (in number of steps).
+            """
+            is_monitor_wrapped = False
+            # Avoid circular import
+            from stable_baselines3.common.monitor import Monitor
+
+            if not isinstance(self.env, VecEnv):
+                self.env = DummyVecEnv([lambda: self.env])  # type: ignore[list-item, return-value]
+
+            is_monitor_wrapped = is_vecenv_wrapped(self.env, VecMonitor) or self.env.env_is_wrapped(Monitor)[0]
+
+            if not is_monitor_wrapped and warn:
+                warnings.warn(
+                    "Evaluation environment is not wrapped with a ``Monitor`` wrapper. "
+                    "This may result in reporting modified episode lengths and rewards, if other wrappers happen to modify these. "
+                    "Consider wrapping environment first with ``Monitor`` wrapper.",
+                    UserWarning,
+                )
+
+            n_envs = self.env.num_envs
+            episode_rewards = []
+            episode_lengths = []
+
+            episode_counts = np.zeros(n_envs, dtype="int")
+            # Divides episodes among different sub environments in the vector as evenly as possible
+            episode_count_targets = np.array([(n_eval_episodes + i) // n_envs for i in range(n_envs)], dtype="int")
+
+            current_rewards = np.zeros(n_envs)
+            current_lengths = np.zeros(n_envs, dtype="int")
+            
+            
+            # Initial reset
+            obs, info = self.pure_env.reset()
+            reward = self.no_reward
+            done = False
+            
+            # Feed with initial observation and info as a result of real environment reset
+            for alg in self.alg_collection:       
+                alg['env'].reset()
+                alg['env'].feedNextStep(obs[alg['obs_index']], 0, False, False, info)
+                alg['states'] = None
+                
+                
+                
+            episode_starts = np.ones((self.env.num_envs,), dtype=bool)
+            
+
+            while (episode_counts < episode_count_targets).any():
+                some_reset = False
+                some_close = False
+                
+                
+                # Execute learn step sending from all algorithms
+                for alg in self.alg_collection:
+                    
+                    actions, alg['states'] = alg['alg'].predict(
+                            obs[alg['obs_index']],  # type: ignore[arg-type]
+                            state=alg['states'],
+                            episode_start=episode_starts,
+                            deterministic=deterministic,
+                    )
+                    
+                    if(done):
+                        alg['env'].reset()
+                    else:
+                        alg['env'].step(actions)
+                    
+                    # Retrieve from virtual environment which action was proposed by algorithm
+                    actions, reset, close = alg['env'].getLastActions()
+                    
+                    # Store reset or close orders
+                    some_reset |= reset
+                    some_close |= close
+                    
+                    # Fill actions
+                    if(alg['discrete']):
+                        for action_i in alg['action_indexes_range']:
+                            action_n = alg['action_indexes'][action_i]
+                            self.action_multibinary[action_n] = (actions == (1 << action_i))
+                    else:
+                        for action_i in alg['action_indexes_range']:
+                            action_n = alg['action_indexes'][action_i]
+                            self.action_multibinary[action_n] = actions[action_i]
+                
+                # Commit reset in case one of all algorithms asked for it (normally when real env was done, trimmed)
+                if(some_reset):
+                    obs, info = self.pure_env.reset(None)
+                    reward, done, trimmed = [self.no_reward, False, False]
+                    for alg in self.alg_collection:
+                        alg['env'].feedNextStep(obs[alg['obs_index']], reward[alg['reward_index']],\
+                            done, trimmed, info)   
+                else:
+                    # Otherwise, do a normal step in real environment with action multibinary array filled actions
+                    obs, reward, done, trimmed, info = self.pure_env.step(self.action_multibinary)
+                    for alg in self.alg_collection:
+                        alg['env'].feedNextStep(obs[alg['obs_index']], reward[alg['reward_index']],\
+                            done, trimmed, info)
+                        
+
+                if render:
+                    self.pure_env.render()
+
+            
+            return 0, 0
         
